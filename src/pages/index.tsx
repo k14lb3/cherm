a54e8d8bc8c1722ac7bb326b0d151db1d0807a70
k14lb3 <k14lb3@gmail.com>
2022-07-18 13:56:12 +0800

    Add Header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
+import Header from '@/components/header';
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +10,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Chat application." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
     </>
   );
 };
