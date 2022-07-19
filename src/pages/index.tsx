b143ee2a858deb4e378b37f24451d6f214e29c0d
k14lb3 <k14lb3@gmail.com>
2022-07-20 01:46:02 +0800

    Fetch then set user ip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
+import useStore from '@/store';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Header from '@/components/header';
 import ChatBox from '@/components/chatbox';
 
 const Home: NextPage = () => {
+  const setIp = useStore((state) => state.setIp);
+
+  useEffect(() => {
+    const getIp = async () =>
+      (await (await fetch('https://ident.me/json')).json()).ip;
+
+    const set = async () => setIp(await getIp());
+
+    set();
+  }, []);
+
   return (
     <>
       <Head>
