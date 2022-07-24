65c538db70f0c81ad2cfa0b3b880fca008d35c17
k14lb3 <k14lb3@gmail.com>
2022-07-24 12:34:59 +0800

    Add Help component

---
+++ src/components/ui/help.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Notification } from '@/components/ui';
+
+const help = [
+  {
+    command: 'search    ',
+    action: 'search for a stranger to chat with',
+  },
+  {
+    command: 'exit      ',
+    action: 'exit chat with stranger',
+  },
+];
+
+interface HelpProps {
+  key?: React.Key;
+}
+
+export const Help: React.FC<HelpProps> = ({ key }) => {
+  return (
+    <React.Fragment key={key}>
+      <Notification
+        className="mb-2"
+        subClassName="break-all"
+        text="usage : cherm <command>"
+      />
+      <Notification
+        className="mb-2"
+        subClassName="break-all"
+        text="commands :"
+      />
+      {help.map(({ command, action }) => (
+        <Notification
+          className="mb-2"
+          subClassName="break-all"
+          text={` ${command}${action}`.replaceAll(' ', '\u00a0')}
+        />
+      ))}
+    </React.Fragment>
+  );
+};
