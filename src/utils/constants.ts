1761329b0c4265acc24717d4e60275701e8d8aa4
k14lb3 <k14lb3@gmail.com>
2022-07-29 23:30:12 +0800

    Add constant disconnect_message variable

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,8 @@ export const command = {
   help: 'cherm help',
   search: 'cherm search',
 };
+
+export const disconnect_message = {
+  me: 'you have disconnected.',
+  stranger: 'stranger has disconnected.',
+};
