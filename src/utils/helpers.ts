931361eb5fc0b8ce9d6ade495046bda780ad5f88
k14lb3 <k14lb3@gmail.com>
2022-07-30 22:11:08 +0800

    Add isCommand helper function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,5 @@
 import { randomBytes } from 'crypto';
+import { Command, command } from '@/utils/constants';
 
 // https://github.com/googleapis/nodejs-firestore/blob/4f4574afaa8cf817d06b5965492791c2eff01ed5/dev/src/util.ts#L52
 export const autoId = (): string => {
@@ -17,4 +18,14 @@ export const autoId = (): string => {
   }
 
   return autoId;
+};
+
+export const isCommand = (input: string) => {
+  const commands = Object.keys(command).map(
+    (key) => command[key as keyof Command],
+  );
+
+  if (commands.includes(input)) return true;
+
+  return false;
 };
