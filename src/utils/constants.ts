67645900d0527e06f153ec8950ba9ea09d7b6541
k14lb3 <k14lb3@gmail.com>
2022-07-30 21:40:25 +0800

    Add types to constant variables

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,23 @@
-export const command = {
+export interface Command {
+  help: string;
+  search: string;
+}
+
+export const command: Command = {
   help: 'cherm help',
   search: 'cherm search',
 };
 
-export const notification = {
+export interface Notification {
+  initial: string;
+  chatting: string;
+  disconnect: {
+    me: string;
+    stranger: string;
+  };
+}
+
+export const notification: Notification = {
   initial: "type 'cherm help' to display available commands.",
   chatting: 'you are now chatting with a random stranger.',
   disconnect: {
