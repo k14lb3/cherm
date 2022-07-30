39ed07ac8786de2c133cac7da76948b745392a43
k14lb3 <k14lb3@gmail.com>
2022-07-30 18:52:47 +0800

    Add chatting property in notification

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,6 +5,7 @@ export const command = {
 
 export const notification = {
   initial: "type 'cherm help' to display available commands.",
+  chatting: 'you are now chatting with a random stranger.',
   disconnect: {
     me: 'you have disconnected.',
     stranger: 'stranger has disconnected.',
